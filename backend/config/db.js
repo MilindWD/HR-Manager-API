97b34abb4e4fafd6b977d27066925075060b4b03
milindmgowda <43924876+milindmgowda@users.noreply.github.com>
2021-06-19 21:27:53 +0530

    Changed DB

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const connection = await mongoose.connect(`mongodb+srv://taskapp:qwerty@123@cluster0.ghhdp.mongodb.net/hr-manager?retryWrites=true`, {
+        const connection = await mongoose.connect(`mongodb+srv://hwhcs:Wellness@68@cluster0.y1ego.mongodb.net/hr-manager?retryWrites=true&w=majority`, {
             useUnifiedTopology: true,
             useNewUrlParser: true,
             useCreateIndex: true
